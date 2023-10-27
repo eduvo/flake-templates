ddfec8dc6fc2817b807b5e1087d9ce294995feac
Yejun Su <goofan.su@gmail.com>
2023-10-27 10:32:48 +0800

    add rails-postgres template

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
 
   outputs = { self }: {
     templates = {
+      rails-postgres = {
+        path = ./rails-postgres;
+        description = "Rails development environment with PostgreSQL";
+      };
       rails-mysql = {
         path = ./rails-mysql;
         description = "Rails development environment with MySQL";
